c132d47a46933a75ac8e486117405509f45c0e1d
Ambert <zhouzixun2000@163.com>
2024-07-29 11:10:52 +0800

    [update] 耗时测试 #34

--- samples/mindX/acleTest.cpp
+++ samples/mindX/acleTest.cpp
@@ -8,40 +8,52 @@ int deviceId, x, y;
 
 void blendImageTest(MxBase::AscendStream& stream) {
   auto st = seeker::time::currentTime();
+  auto t = seeker::time::currentTime();
   //加载素材图片
   cv::Mat srcMatHost = cv::imread("21.png", cv::IMREAD_UNCHANGED);
   cv::cvtColor(srcMatHost, srcMatHost, cv::COLOR_BGRA2RGBA);
   cv::resize(srcMatHost, srcMatHost, cv::Size(300, 300));
 
-  I_LOG("load source picture success");
+  I_LOG("load source picture success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
 
   //加载背景图片
   cv::Mat bottomMatHost = cv::imread("bottom.png", cv::IMREAD_COLOR);
   cv::cvtColor(bottomMatHost, bottomMatHost, cv::COLOR_BGR2RGB);
-  I_LOG("load background picture success");
+
+  I_LOG("load background picture success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
 
   //上传素材图片
   acle::GpuMat srcMatGpu(srcMatHost);
+  I_LOG("upload source picture success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
 
   //上传背景图片
   acle::GpuMat bottomMatGpu(bottomMatHost);
+  I_LOG("upload background picture success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
 
   //透明图片叠加
   acle::GpuMat dstMatGpu;
   if (acle::overlayGpuAlpha(srcMatGpu, bottomMatGpu, dstMatGpu, x, y, stream) != 0) return;
 
-  I_LOG("blend tensor success");
+  I_LOG("blend tensor success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
 
   //下载结果张量至Host侧
   cv::Mat dstMat;
   dstMatGpu.download(dstMat, stream);
   stream.Synchronize();
+  I_LOG("download dst picture success, use {}ms", seeker::time::currentTime() - t);
+  t = seeker::time::currentTime();
+
   cv::Mat cvtMat;
   cv::cvtColor(dstMat, cvtMat, cv::COLOR_RGB2BGR);
   cv::imwrite("dst.png", cvtMat);
 
-  I_LOG("write dst mat success");
-  I_LOG("run time {} ms", seeker::time::currentTime() - st);
+  I_LOG("write dst mat success, use {}ms", seeker::time::currentTime() - t);
+  I_LOG("run time use {} ms", seeker::time::currentTime() - st);
 }
 
 int main(int argc, char* argv[]) {
@@ -60,8 +72,8 @@ int main(int argc, char* argv[]) {
     MxBase::AscendStream stream(deviceId);
     stream.CreateAscendStream();
     blendImageTest(stream);
-    //blendImageTest(stream);
-    //blendImageTest(stream);
+    blendImageTest(stream);
+    blendImageTest(stream);
 
   }
   MxBase::MxDeInit();
